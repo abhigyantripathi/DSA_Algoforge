6d55d89ddcd3cecd5187773e950737fb76faaf21
abhigyantripathi <abhigyantripathi2021@gmail.com>
2026-09-01 21:46:21 +0530

    backtrackingg

--- Backtracking/Intro/main.java
+++ Backtracking/Intro/main.java
@@ -24,12 +24,72 @@ public class main {
         visited[row][col] = false; // backtracking -> will allow current cell to be part of some other recursive path 
     }
 
+    public static void printVisitedMatrix(boolean[][] vis){
+        for(int i=0;i<vis.length;i++){
+            for(int j=0;j<vis[0].length;j++){
+                if(vis[i][j]==true){
+                    System.out.print("(" + i + "," + j + ")");
+                }
+            }
+        }
+    }
+    public static boolean isPossibleToPlaceHere(int row, int col, boolean[][] vis) {
+        int n=vis.length;
+        int[][] dirs={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1}};
+        for(int radius=1 ; radius<n ; radius++){
+            for(int[] dir: dirs){
+            int nRow= row+ radius * dir[0];
+            int nCol= col+ radius * dir[1];
+            if(nRow>=0 && nCol>=0 && nRow<n && nCol<n && (vis[nRow][nCol]==true)){
+                return false;
+            }
+
+            }
+
+
+        }
+
+        return true;
+        
+    }
+
+    public static void NQueens(int row, int n, boolean[][] vis) {
+        if(row==n){
+            printVisitedMatrix(vis);
+            return;
+        }
+
+
+
+        for(int col=0;col<n;col++){
+            if(isPossibleToPlaceHere(row,col,vis)==true){
+                vis[row][col]=true;
+            }
+            NQueens(row+1, n, vis);
+
+
+            vis[row][col]=false;
+        }
+        
+    }
+
+
+
+
     public static void main(String[] args){
+       /*
         int[][] matrix = {{0,0,0,1,0},{0,1,0,1,0},{0,0,0,0,0},{0,1,0,1,0},{0,1,0,1,0},{0,0,0,1,0}};
         int n=matrix.length;
         int m=matrix[0].length;
         boolean[][] visited= new boolean[][];
         floodFill(0, 0, n, m, matrix, visited, "");
+
+        */
+       
+       int n=4;
+       boolean[][] vis= new boolean[n][n];
+       NQueens(0,n,vis);
+
     }
     
 }
